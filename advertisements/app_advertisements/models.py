7ea93513e10f14719b6c029a29a08243d473f65a
FlamethrowerV <artem.pichugin07@mail.ru>
2023-08-24 16:11:06 +0300

    ModelForm and title

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -18,24 +18,27 @@ class Advertisement(models.Model):
 
     @admin.display(description="дата создания")
     def created_date(self):
-         from django.utils import timezone
-         if self.created_at.date()==timezone.now().date():
-             created_time = self.created_at.time().strftime("%H:%M:%S")
-             return format_html('<span style="color:green; font-weight: bold">СЕГОДНЯ В {}</span>', created_time)
-         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+        from django.utils import timezone
+        if self.created_at.date()==timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color:green; font-weight: bold">СЕГОДНЯ В {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
     @admin.display(description="дата обновления")
     def updated_date(self):
-         from django.utils import timezone
-         if self.updated_at.date()==timezone.now().date():
-             created_time = self.created_at.time().strftime("%H:%M:%S")
-             return format_html('<span style="color:red; font-weight: bold">СЕГОДНЯ В {}</span>', created_time)
-         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+        from django.utils import timezone
+        if self.updated_at.date()==timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color:red; font-weight: bold">СЕГОДНЯ В {}</span>', created_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
     @admin.display(description='фото')
     def get_html_image(self):
         if self.image:
             return format_html(
                 '<img src="{url}" style="max-width: 80px; max-height: 80px;"', url=self.image.url
             )
+
+
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
     class Meta:
